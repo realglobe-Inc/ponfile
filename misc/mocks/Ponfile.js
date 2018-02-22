6cd459cc3e82951d5a262c2682ee0888452be1da
okunishinishi <okunishinishi@gmail.com>
2018-02-22 10:43:03 +0900

    bugfix

--- misc/mocks/Ponfile.js
+++ misc/mocks/Ponfile.js
@@ -1,9 +1,26 @@
 'use strict'
 
-module.exports = {
-  foo () {},
-  bar () {},
+const ponRunner = require('pon-runner')
+const fs = require('fs')
+
+module.exports = ponRunner({
+  writeFoo () {
+    fs.writeFileSync(`${__dirname}/foo.txt`, 'This is foo')
+  },
+  a () {
+    console.log('This is A')
+  },
+  b () {
+    console.log('This is B')
+  },
+  c: ['a', 'b', () => {}],
+  invalid01: ['__invalid_pointer_01__', () => {}],
+  invalid02: ['__invalid_pointer_02__'],
+
   $doc: {
-    foo: 'This is foo'
+    tasks: {
+      a: 'This is a'
+    }
   }
-}
+}).bind()
+
